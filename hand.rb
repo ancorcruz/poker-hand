cf993cb705873b34cc7d9a281c64dee3b6beffc7
Ancor Cruz <hello@ancorcruz.com>
2013-11-14 12:15:44 +0000

    Refactored matchers to allow highest cards inside ranks Added four of a kind matcher

--- hand.rb
+++ hand.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 require 'debugger'
 
 class Hand
-  attr_reader :cards
+  attr_reader :cards, :high_card_in_rank
 
   CARDS_PER_HAND = 5
 
@@ -16,44 +16,49 @@ class Hand
   end
 
   def rank
-    rank_key = RANKS.keys.detect { |rank| send "#{rank}?" }
-    rank_str = rank_key ? "#{RANKS[rank_key]}" : "High Card"
-    "#{rank_str} (#{high_card})"
+    rank_key = RANKS.keys.detect { |rank| send "match_#{rank}" }
+    "#{RANKS[rank_key]} (#{@high_card_in_rank})"
   end
 
   RANKS = {
     straight_flush: "Straight Flush",
-    #four_of_a_kind: "Four of a Kind",
+    four_of_a_kind: "Four of a Kind",
     #full_house: "Full House",
     flush: "Flush",
     straight: "Straight",
     #three_of_a_kind: "Three of a Kind",
     #double_pair: "Two Pairs",
     #pair: "Pair",
+    high_card: "High Card"
   }
 
 
   protected
 
-  def high_card
-    @cards.last
+  def match_high_card
+    @high_card_in_rank = @cards.last
   end
 
-  def straight_flush?
-    straight? and flush?
+  def match_straight_flush
+    match_high_card if match_straight and match_flush
   end
 
-  def flush?
+  def match_flush
     suit = @cards.first.suit
-    @cards.all? { |card| card.suit == suit }
+    match_high_card if @cards.all? { |card| card.suit == suit }
   end
 
-  def straight?
+  def match_straight
     values = @cards.map(&:value)
-    values == (values.first..values.last).entries
+    match_high_card if values == (values.first..values.last).entries
   end
 
-  def four_of_a_kind?
-    values = @cards.map(&:value)
+  def match_four_of_a_kind
+    kind_set = @cards.inject(nil) do |result, card|
+      set = @cards.select { |c| c.value == card.value }
+      set.size == 4 ? set : result
+    end
+
+    @high_card_in_rank = kind_set.first if kind_set
   end
 end
