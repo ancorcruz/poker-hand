7251a69ebb2d2823e9098d8921e2849319159243
Ancor Cruz <hello@ancorcruz.com>
2013-11-14 11:51:27 +0000

    Added first hand matcher for straight flush

--- hand.rb
+++ hand.rb
@@ -1,2 +1,55 @@
+require './card'
+
+require 'rubygems'
+require 'debugger'
+
 class Hand
+  attr_reader :cards
+
+  CARDS_PER_HAND = 5
+
+  def initialize hand_str
+    hand = hand_str.split(" ")
+    raise ArgumentError, "should have #{CARDS_PER_HAND} cards" if hand.count != CARDS_PER_HAND
+
+    @cards = hand.inject([]) { |hand, card| hand << Card.new(card) }.sort
+  end
+
+  def rank
+    rank_key = RANKS.keys.detect { |rank| send "#{rank}?" }
+    rank_str = rank_key ? "#{RANKS[rank_key]}" : "High Card"
+    "#{rank_str} (#{high_card})"
+  end
+
+  RANKS = {
+    straight_flush: "Straight Flush",
+    #four_of_a_kind: "Four of a Kind",
+    #full_house: "Full House",
+    #flush: "Flush",
+    #straight: "Straight",
+    #three_of_a_kind: "Three of a Kind",
+    #double_pair: "Two Pairs",
+    #pair: "Pair",
+  }
+
+
+  protected
+
+  def high_card
+    @cards.last
+  end
+
+  def straight_flush?
+    straight? and flush?
+  end
+
+  def flush?
+    suit = @cards.first.suit
+    @cards.all? { |card| card.suit == suit }
+  end
+
+  def straight?
+    values = @cards.map(&:value)
+    values == (values.first..values.last).entries
+  end
 end
--- hand_test.rb
+++ hand_test.rb
@@ -2,8 +2,19 @@ require 'test/unit'
 require './hand'
 
 class HandTest < Test::Unit::TestCase
+  def test_hands_with_no_5_cards_raise_argument_error
+    assert_raise ArgumentError do
+      Hand.new("TC 4S")
+    end
+  end
+
+  def test_straight_flush
+    hand = Hand.new("TC 8C JC 7C 9C")
+    assert_equal "Straight Flush (JC)", hand.rank
+  end
+
   def test_high_card_rank
-    hand = Hand.new(["3C 4C 8H 9S AD"])
+    hand = Hand.new("3C 4C 8H 9S AD")
     assert_equal "High Card (AD)", hand.rank
   end
 
